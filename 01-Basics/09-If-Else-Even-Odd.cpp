f8edd78e230c7f9ccd73327949794fc88b75c1d4
Zaid Momin <your_email@gmail.com>
2026-08-19 13:31:59 +0530

    Add even odd if else practice

---
+++ 01-Basics/09-If-Else-Even-Odd.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void if_else_eo() {
+        int n;
+        cin >> n;
+
+        if(n % 2 == 0) {
+            cout << "Even" << '\n';
+        }
+        else {
+            cout << "Odd" << '\n';
+        }
+    }
+};
