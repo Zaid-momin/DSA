b2af46a3545cb6fdbb0f4b1351819111dddbb834
Zaid Momin <your_email@gmail.com>
2026-08-19 13:28:08 +0530

    Add positive negative if else practice

---
+++ 01-Basics/08-If-Else-Positive-Negative.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    void if_else_exercise(){
+        int n;
+        cin >> n;
+
+        if(n==0){
+            cout << "Zero"<<'\n';
+        }
+        else if(n>0){
+            cout << "Positive"<<'\n';
+
+        }
+        else{
+            cout << "negative"<<'\n';
+        }
+    }
+};
