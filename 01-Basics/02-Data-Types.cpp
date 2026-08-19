3bab8d6257d359478acb8d3b37bb2d4217bff86f
Zaid Momin <your_email@gmail.com>
2026-08-19 12:18:12 +0530

    Add basic C++ DSA problems

---
+++ 01-Basics/02-Data-Types.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void printing_datatypes() {
+        int a = 10;
+        long long b = 100000;
+        float c = 10.5;
+        double d = 1005.5;
+        char e = 'A';
+        string s = "Zaid";
+
+        cout << a << '\n';
+        cout << b << '\n';
+        cout << c << '\n';
+        cout << d << '\n';
+        cout << e << '\n';
+        cout << s;
+    }
+};
